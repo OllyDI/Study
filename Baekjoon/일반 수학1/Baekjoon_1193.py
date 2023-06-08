d0262eb90174f1c6fc3c4fb3fe107b6e4f9723c3
Seungho Choi <choiseungho11@hanmail.net>
2023-06-08 17:37:51 +0900

    23.06.08

--- Baekjoon/일반 수학1/Baekjoon_1193.py
+++ Baekjoon/일반 수학1/Baekjoon_1193.py
@@ -62,3 +62,19 @@ X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시�
 예제 출력 10 
 2/4
 """
+
+x = int(input())
+line = 1
+
+while x > line:
+    x -= line
+    line += 1
+
+if line % 2 == 0:
+    up = x
+    down = line - x + 1
+else:
+    up = line - x + 1
+    down = x
+
+print(up, '/', down, sep="")
