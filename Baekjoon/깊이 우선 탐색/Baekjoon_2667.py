4e9c516d9a49b6fd6559f11f05bcbaaa1cbdea4a
Seungho Choi <choiseungho11@hanmail.net>
2024-01-08 14:00:44 +0900

    23.01.08

--- Baekjoon/깊이 우선 탐색/Baekjoon_2667.py
+++ Baekjoon/깊이 우선 탐색/Baekjoon_2667.py
@@ -50,7 +50,6 @@ def dfs(x, y):
         for i in range(4):
             dx = [1, -1, 0, 0]
             dy = [0, 0, 1, -1]
-
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n: dfs(nx, ny)
