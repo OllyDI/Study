380a2dad6cffd1d387dc642e47ec47957d7628cd
OllyDI <choiseungho11@hanmail.net>
2025-02-16 15:33:53 +0900

    25.02.16

--- Baekjoon/동적 계획법 1/Baekjoon_1463.py
+++ Baekjoon/동적 계획법 1/Baekjoon_1463.py
@@ -26,8 +26,23 @@ X가 2로 나누어 떨어지면, 2로 나눈다.
 3
 """
 
+# n = int(input())
+# arr = [0] * (n + 1)
+
+# for i in range(2, n + 1):
+#     arr[i] = arr[i - 1] + 1
+
+#     if i % 3 == 0: arr[i] = min(arr[i], arr[i // 3] + 1)
+#     if i % 2 == 0: arr[i] = min(arr[i], arr[i // 2] + 1)
+# print(arr[n])
+
+# 복습
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = [0] * (n + 1)
+arr = [0] * n + 1
 
 for i in range(2, n + 1):
     arr[i] = arr[i - 1] + 1
