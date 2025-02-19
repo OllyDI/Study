ecf18899bc9175cebc53301f6f5dec3691b1b4ab
OllyDI <choiseungho11@hanmail.net>
2025-02-19 13:09:10 +0900

    25.02.19

--- Baekjoon/동적 계획법 1/Baekjoon_11726.py
+++ Baekjoon/동적 계획법 1/Baekjoon_11726.py
@@ -42,11 +42,25 @@
 #         num2 = res
 #     print(res % 10007)
 
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# dp = [0, 1, 2]
+
+# for i in range(3, n + 1): dp.append(dp[i - 1] + dp[i - 2])
+# print(dp[n] % 10007)
+
+
+# 복습 문제를 n = 1 부터 풀어서 보면 dp[n] = dp[n - 1] + dp[n - 2]가 됨
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 dp = [0, 1, 2]
 
-for i in range(3, n + 1): dp.append(dp[i - 1] + dp[i - 2])
+for i in range(3, n + 1):
+    dp.append(dp[i - 1] + dp[i - 2])
+
 print(dp[n] % 10007)
